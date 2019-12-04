04e9ae9b2a58f293ee2457f26eb67b692ea7742b
Nuttapong <thenuts53@gmail.com>
2019-12-04 13:22:41 +0700

    test build app in jenkins

--- app/src/main/java/com/scode/admin/diceroller/MainActivity.kt
+++ app/src/main/java/com/scode/admin/diceroller/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
@@ -24,9 +23,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
